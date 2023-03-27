cd8f6b34d16ce0d028ebacc4f51f632bb51f1aae
xxpg520 <xxpg@qq.com>
2023-03-27 16:16:09 +0800

    字典的常用操作

--- 第六章 数据容器/15_字典的常用操作.py
+++ 第六章 数据容器/15_字典的常用操作.py
@@ -8,22 +8,22 @@ my_dict["张信哲"] = 66
 print(f"字典经过新增元素，结果：{my_dict}")
 # 字典经过新增元素，结果：{'周杰轮': 99, '林俊节': 88, '张学油': 77, '张信哲': 66}
 
-# 更新元素
+# 更新元素   字典[key] = value值
 my_dict["周杰轮"] = 33
 print(f"字典经过更新后，结果是：{my_dict}")
 # 字典经过更新后，结果是：{'周杰轮': 33, '林俊节': 88, '张学油': 77, '张信哲': 66}
 
-# 删除元素
+# 删除元素    字典.pop(key)
 score = my_dict.pop("周杰轮")
 print(f"字典中被移除了一个元素，结果{my_dict}，周杰轮的考试分数是：{score}")
 # 字典中被移除了一个元素，结果{'林俊节': 88, '张学油': 77, '张信哲': 66}，周杰轮的考试分数是：33
 
-# 清空元素，clear
+# 清空元素，clear   字典.clear()
 my_dict.clear()
 print(f"字典被清空了，内容是：{my_dict}")
 # 字典被清空了，内容是：{}
 
-# 获取全部的key
+# 获取全部的key  字典.keys()
 my_dict = {"周杰轮": 99, "林俊节": 88, "张学油": 77}
 keys = my_dict.keys()
 print(f"字典的全部keys是：{keys}")
@@ -34,4 +34,18 @@ print(f"字典的全部keys是：{keys}")
 for key in keys:
     print(f"字典的key是：{key}")
     print(f"字典的value是：{my_dict[key]}")
+# 方式2：直接对字典进行for循环，每一次循环都是直接得到key
+for key in my_dict:
+    print(f"2字典的key是：{key}")
+    print(f"2字典的value是：{my_dict[key]}")
+# 字典的key是：周杰轮
+# 字典的value是：99
+# 字典的key是：林俊节
+# 字典的value是：88
+# 字典的key是：张学油
+# 字典的value是：77
 
+# 统计字典内的元素数量，len()函数   字典.len()
+num = len(my_dict)
+print(f"字典中的元素数量有：{num}个")
+# 字典中的元素数量有：3个
