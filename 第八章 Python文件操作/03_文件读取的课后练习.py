24cdcae29d28f623c497ca111c75179bc035f67a
xxpg <xxpg@qq.com>
2023-03-29 23:20:44 +0800

    文件读取的课后练习

--- 第八章 Python文件操作/03_文件读取的课后练习.py
+++ 第八章 Python文件操作/03_文件读取的课后练习.py
@@ -1,9 +1,5 @@
 with open("C:/Users/xxpg/Desktop/word.txt", 'r', encoding="UTF-8") as f:
-    f = f.read()
-print(f"每行数据:{f}")
-num = None
-for itheima in f:
-    if itheima == "itheima":
-        num += 1
-print(num)
+    num = f.read().count("itheima")
+print(f"word.txt中共有itheima: {num}个")
+
 
