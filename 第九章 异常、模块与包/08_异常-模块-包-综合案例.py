56625e65a683025c3b7c9ad57c34742b9a7e1995
xxpg520 <xxpg@qq.com>
2023-04-04 16:57:41 +0800

    异常 模块 包 综合案例

--- 第九章 异常、模块与包/08_异常-模块-包-综合案例.py
+++ 第九章 异常、模块与包/08_异常-模块-包-综合案例.py
@@ -10,5 +10,3 @@ flie_util.py (文件处理相关工具, 内含:)
     函数: print_file_info(file_name), 接收传入文件的路径, 打印文件的全部内容, 如文件不存在则捕获异常, 输出提示信息,通过finally关闭文件对象
     函数: append_to_file(file_name, data), 接收文件路径以及传入数据,将数据追加写入到文件中
 """
-from my_utils import *
-sub_str()
