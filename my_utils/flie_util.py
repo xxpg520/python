d8792f842f5b7b83e1baee3c06ef369cfbbc31ec
xxpg520 <xxpg@qq.com>
2023-04-04 16:51:07 +0800

    异常 模块 包 综合案例

---
+++ my_utils/flie_util.py
@@ -0,0 +1,15 @@
+# 函数: str_reverse(s), 接受传入字符串, 将字符串反转返回
+# 函数: sub_str(s, x, y), 按照下标x和y,对字符串进行切片
+
+def str_reverse():
+    str1 = str(input("请输入任意字符串:"))
+    str2 = str1[::-1]
+    return str2
+
+
+def sub_str():
+    x = int(input("请输入下标起始:"))
+    y = int(input("请输入下标起始:"))
+    for star in str_reverse():
+        s = []
+
