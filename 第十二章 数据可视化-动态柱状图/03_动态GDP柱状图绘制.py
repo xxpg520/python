47d4d8e4145217aaf293f2950f07bf5ad27f68ff
xxpg520 <xxpg@qq.com>
2023-04-11 16:08:05 +0800

    GDP动态柱状图

--- 第十二章 数据可视化-动态柱状图/03_动态GDP柱状图绘制.py
+++ 第十二章 数据可视化-动态柱状图/03_动态GDP柱状图绘制.py
@@ -14,4 +14,23 @@ data_lines.pop(0)
 # {1960:[[美国, 123],[中国，321],…………]，1961:[[美国,gdp],[中国，321]…………]…………}
 # 先定义一个字典对象
 data_dict = {}
-#
+for line in data_lines:
+    year = int(line.split(",")[0])      # 年份
+    country = line.split(",")[1]        # 国家
+    gdp = float(line.split(",")[2])     # gdp数据
+    # 如何判断字典里面有没有key呢?
+
+    try:
+        data_dict[year].append([country, gdp])
+    except KeyError:
+        data_dict[year] =[]
+        data_dict[year].append([country, gdp])
+
+# 排序年份
+sorted_year_list = data_dict.keys()
+for year in sorted_year_list:
+    data_dict[year].sort(key=lambda element: element[1],reverse=True)
+    # 取出本年份前8名的国家
+    year_data = data_dict[year][0:8]
+    x_data = []
+    y_data = []
