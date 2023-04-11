bfc49f8190bbc3a73dd950760b9b8c7677f6d522
xxpg520 <xxpg@qq.com>
2023-04-11 09:16:38 +0800

    GDP动态柱状图

--- 第十二章 数据可视化-动态柱状图/03_动态GDP柱状图绘制.py
+++ 第十二章 数据可视化-动态柱状图/03_动态GDP柱状图绘制.py
@@ -1,7 +1,6 @@
 """
 演示第三个图标：GDP动态柱状图开发
 """
-from pandas.core.internals.construction import dataclasses_to_dicts
 
 # 读取数据
 f = open("D:/1960-2019全球GDP数据.csv", "r", encoding="GB2312")
@@ -15,3 +14,4 @@ data_lines.pop(0)
 # {1960:[[美国, 123],[中国，321],…………]，1961:[[美国,gdp],[中国，321]…………]…………}
 # 先定义一个字典对象
 data_dict = {}
+#
