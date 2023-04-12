9a42d0501dd04d7c678f8e06b669d9b87a4e6a95
xxpg <xxpg@qq.com>
2023-04-12 22:43:37 +0800

    类中成员方法的定义和使用

--- 第二阶段/第一章/02_类的成员方法.py
+++ 第二阶段/第一章/02_类的成员方法.py
@@ -4,11 +4,17 @@
 
 # 定义一个带有成员方法的类
 class Student:
-    name = None    # 定义成员变量
-# 定义成员方法
+    name = None    # 学生的姓名
+    # 定义成员方法
     def say_hi(self):
-        print(f"大家好啊,我是{self.name},欢迎大家多多关照")
+        print(f"大家好啊，我是{self.name}，欢迎大家多多关照")
 
+    def say_hi2(self, msg):
+        print(f"大家好，我是：{self.name},{msg}欢迎大家多多关照")
 stu = Student()
 stu.name = "周杰轮"
-stu.say_hi()
+stu.say_hi2("哎呦不错呦")
+
+stu2 = Student()
+stu2.name = "林俊节"
+stu2.say_hi2("小伙子我看好你")
