6c3bb6ea7abf09c55b469bc9adc47fc480fdf273
xxpg <xxpg@qq.com>
2023-04-12 20:53:52 +0800

    出识对象

--- 第二阶段/第一章/01_初识对象.py
+++ 第二阶段/第一章/01_初识对象.py
@@ -5,18 +5,17 @@ class Student:
     nationality = None      # 记录学生国籍
     native_place = None     # 记录学生机关
     age = None              # 记录学生年龄
-
 # 2.创建一个对象（类比生活中：打印一张登记表）
-stu1 = Student()
+stu_1 = Student()
 # 3.对象属性进行赋值（类比生活中：填写表单）
-stu1.name = "林军杰"
-stu1.gender = "男"
-stu1.nationality = "中国"
-stu1.native_place = "山东省"
-stu1.age = 31
+stu_1.name = "林军杰"
+stu_1.gender = "男"
+stu_1.nationality = "中国"
+stu_1.native_place = "山东省"
+stu_1.age = 31
 # 4.获取对象记录的信息
-print(stu1.name)
-print(stu1.gender)
-print(stu1.nationality)
-print(stu1.native_place)
-print(stu1.age)
+print(stu_1.name)
+print(stu_1.gender)
+print(stu_1.nationality)
+print(stu_1.native_place)
+print(stu_1.age)
