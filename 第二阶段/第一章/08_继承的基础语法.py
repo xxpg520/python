e65a84f77a28918e7cd894f454c2849fc5a8c605
xxpg520 <xxpg@qq.com>
2023-04-14 15:56:17 +0800

    继承的基础语法

---
+++ 第二阶段/第一章/08_继承的基础语法.py
@@ -0,0 +1,53 @@
+"""
+演示面向对象：继承的基础语法
+"""
+
+# 演示单继承
+class Phone:
+    IMEI = None     # 序列号
+    producer = "ITCAST"
+
+    def call_by_4g(self):
+        print("4g通话")
+
+class Phone2022(Phone):
+    face_id = "10001"       # 面部识别ID
+
+    def call_by_5g(self):
+        print("2022年新功能：5g通话")
+
+phone = Phone2022()
+print(phone.producer)
+phone.call_by_4g()
+phone.call_by_5g()
+# 演示多继承
+class NFCReader:
+    nfc_type = "第五代"
+    producer = "HM"
+
+    def read_card(self):
+        print("NFC读卡")
+
+    def write_card(self):
+        print("NFC写卡")
+
+class RemoteControl:
+    rc_type = "红外遥控"
+
+    def control(self):
+        print("红外遥控开启了")
+
+class MyPhone(Phone, NFCReader, RemoteControl):
+    pass
+
+phone = MyPhone()
+phone.call_by_4g()
+phone.read_card()
+phone.write_card()
+phone.control()
+
+
+
+# 演示多继承下，父类成员名一致的场景
+# Phone NFCReader 都有producer 先继承的优先
+print(phone.producer)
