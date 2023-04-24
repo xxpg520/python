817c4a0039e8ed899537c29ae8855a866d3a499f
xxpg <xxpg@qq.com>
2023-04-25 00:34:15 +0800

    列表操作 插入 删除 更新

--- 第二阶段/第二章 SQL/Python操作MySql基础操作.py
+++ 第二阶段/第二章 SQL/Python操作MySql基础操作.py
@@ -8,23 +8,31 @@ conn = Connection(
     host="localhost",        # 主机名(ip)
     port=3306,               # 端口
     user="root",             # 帐户
-    password="samsung2166"   # 密码
+    password="***********",  # 密码
+    autocommit=True          # 自动提交（确认）
 )
 
 # print(conn.get_server_info())
 # 执行非查询性SQL
-cursor = conn.cursor()      # 获取到游标对象
+# cursor = conn.cursor()      # 获取到游标对象
 # 选择数据库
-conn.select_db("test")
+# conn.select_db("test")
 # 执行sql
 # cursor.execute("create table test_pymysql(id int);")    # 执行("")内sql语句
 
 # 执行查询性SQL
-conn.select_db("world")
-cursor.execute("select * from student;")
-results = cursor.fetchall()
-for r in results:
-    print(r)
+# cursor = conn.cursor()                                 # 获取到游标对象
+# conn.select_db("world")                                # 选择数据库
+# cursor.execute("select * from student;")               # 执行（）内语句
+# results = cursor.fetchall()
+# for r in results:
+#     print(r)
+
+# 执行非查询性SQL
+cursor = conn.cursor()                                                        # 获取到游标对象
+conn.select_db("world")                                                       # 选择数据库
+cursor.execute("insert into student values(10002,'林俊节',31,'男');")           # 执行（）内语句
+# conn.commit()                                                                 # 提交
 
 # 关闭链接
 conn.close()
