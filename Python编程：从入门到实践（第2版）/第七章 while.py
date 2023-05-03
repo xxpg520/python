593bf40f3f5716cb4646f1497d9d5e1f8b1ebc51
xxpg <xxpg@qq.com>
2023-05-03 15:23:26 +0800

    循环练习

--- Python编程：从入门到实践（第2版）/第七章 while.py
+++ Python编程：从入门到实践（第2版）/第七章 while.py
@@ -1,4 +1,40 @@
 # """
+# 练习7-4：比萨配料
+# 编写一个循环，提示用户输入一系列比萨配料，并在用户输入'quit' 时结束循环。
+# 每当用户输入一种配料后，都打印一条消息，指出我们会在比萨中添加这种配料。
+# """
+# active = True
+# pizaas = []
+# while active:
+#     配料 = input("请输入你想要的配料：")
+#     if 配料 == 'quit':
+#         active = False
+#     else:
+#         pizaas.append(配料)
+#         print(f"您要添加的{配料}已经添加到您的披萨中。")
+#
+# pizaa = '、'.join(pizaas)
+# print(f"我们会在比萨中添加：{pizaa}")
+
+"""
+练习7-5：电影票
+有家电影院根据观众的年龄收取不同的票价：不到3岁的观众免费；3～12岁的观众收费10美元；超过12岁的观众收费15美元。
+请编写一个循环，在其中询问用户的年龄，并指出其票价。
+"""
+active = True
+while active:
+    age = int(input("请输入您的年龄："))
+    if age < 3:
+        print(f"您的年龄为：{age},您不足3岁，可免费观影。")
+    elif 3 <= age <= 12:
+        print(f"您的年龄为：{age},属于少年，可按优惠价10元购票观影。")
+    elif age > 12:
+        print(f"您的年龄为：{age}，已经成年，应按全价15元购票观影。")
+    else:
+        print("请输入正确年龄！")
+
+
+# """
 # 练习7-8：熟食店
 # 创建一个名为sandwich_orders 的列表，在其中包含各种三明治的名字，再创建一个名为finished_sandwiches 的空列表。
 # 遍历列表sandwich_orders ，对于其中的每种三明治，都打印一条消息，如I made your tuna sandwich ，并将其移到列表finished_sandwiches 中。
