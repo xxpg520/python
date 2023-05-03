20278d46cd347b0b4a1f235a3132856de77133b9
xxpg <xxpg@qq.com>
2023-05-03 23:01:16 +0800

    函数练习。

--- Python编程：从入门到实践（第2版）/第八章 函数.py
+++ Python编程：从入门到实践（第2版）/第八章 函数.py
@@ -53,3 +53,10 @@
 # describe_city('kunming', 'china')
 # describe_city('tokyo', 'japan')
 # describe_city('new york', 'america')
+
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名。"""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
