955b438aeed4d291a62462efa3cd93b28eb8500c
xxpg520 <xxpg@qq.com>
2023-05-04 16:57:50 +0800

    函数练习

--- Python编程：从入门到实践（第2版）/第八章 函数.py
+++ Python编程：从入门到实践（第2版）/第八章 函数.py
@@ -54,9 +54,23 @@
 # describe_city('tokyo', 'japan')
 # describe_city('new york', 'america')
 
-def get_formatted_name(first_name, last_name):
-    """返回整洁的姓名。"""
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
+"""
+练习8-6：城市名
+编写一个名为city_country() 的函数，它接受城市的名称及其所属的国家。这个函数应返回一个格式类似于下面的字符串：
+"Santiago, Chile"
+至少使用三个城市国家对来调用这个函数，并打印它返回的值。
+"""
+
+"""
+练习8-7：专辑 
+编写一个名为make_album() 的函数，它创建一个᧿述音乐专辑的字典。这个函数应接受歌手的名字和专辑名，并返回一个包含这两项信息的字典。
+使用这个函数创建三个表示不同专辑的字典，并打印每个返回的值，以核实字典正确地存储了专辑的信息。
+    给函数make_album() 添加一个默认值为None 的可选形参，以便存储专辑包含的歌曲数。如果调用这个函数时指定了歌曲数，
+    就将该值添加到表示专辑的字典中。调用这个函数，并至少在一次调用中指定专辑包含的歌曲数.
+"""
+
+"""
+练习8-8：用户的专辑 
+在为完成练习8-7编写的程序中，编写一个while 循环，让用户输入专辑的歌手和名称。获取这些信息后，
+使用它们来调用函数make_album() 并将创建的字典打印出来。在这个while 循环中，务必提供退出途径。
+"""
