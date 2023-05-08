9ee059c3a95ac454af6f66d9a0729ebd285ddf4a
xxpg520 <xxpg@qq.com>
2023-05-08 16:55:42 +0800

    类-练习

--- Python编程：从入门到实践（第2版）/第九章 类/car.py
+++ Python编程：从入门到实践（第2版）/第九章 类/car.py
@@ -1,3 +1,5 @@
+"""一组用于表示燃油汽车和电动汽车的类。"""
+
 class Car:
     """一次模拟汽车的简单尝试"""
 
@@ -30,12 +32,3 @@ class Car:
     def increment_odometer(self, miles):
         """将里程表读书增加指定的量"""
         self.odometer_reading += miles
-
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
