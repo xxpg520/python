1635ea0ba1152bfb355c260ae80ed448fad37109
xxpg <xxpg@qq.com>
2023-05-29 01:39:18 +0800

    文件和异常

--- Python编程：从入门到实践（第2版）/第十章 文件和异常/pi_string.py
+++ Python编程：从入门到实践（第2版）/第十章 文件和异常/pi_string.py
@@ -1,4 +1,4 @@
-filename = 'pi_digits.txt'
+filename = 'pi_million_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
@@ -7,5 +7,5 @@ pi_string = ''
 for line in lines:
     pi_string += line.strip()
 
-print(pi_string)
+print(f"{pi_string[:]}...")
 print(len(pi_string))
