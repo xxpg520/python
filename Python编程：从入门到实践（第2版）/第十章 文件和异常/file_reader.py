c29e8584994210f0df05e9ec883332248d905224
xxpg <xxpg@qq.com>
2023-05-28 11:21:30 +0800

    文件和异常

--- Python编程：从入门到实践（第2版）/第十章 文件和异常/file_reader.py
+++ Python编程：从入门到实践（第2版）/第十章 文件和异常/file_reader.py
@@ -1,5 +1,7 @@
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
-	for line in file_object:
-		print(line.rstrip())
+	lines = file_object.readlines()
+
+for line in lines:
+	print(line.rstrip())
