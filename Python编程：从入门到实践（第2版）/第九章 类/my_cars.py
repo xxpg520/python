6109e4652f94d6d81a97c136d24769f1e9a90425
xxpg520 <xxpg@qq.com>
2024-04-20 22:02:27 +0800

    1

--- Python编程：从入门到实践（第2版）/第九章 类/my_cars.py
+++
@@ -1,8 +0,0 @@
-from electric_car import ElectricCar as EC
-from car import Car
-
-my_beetle = Car('volkswagen', 'beetle', 2019)
-print(my_beetle.get_descriptive_name())
-
-my_tesla = EC('tesla', 'roadster', 2019)
-print(my_tesla.get_descriptive_name())
--- 第一阶段/第九章 异常、模块与包/my_module1.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['test_a']
-def test_a(a, b):
-    print(a + b)
-
-def test_b(a, b):
-    print(a - b)
-
-
